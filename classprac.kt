a3bf20266759366d2ac00c0f2fcc4e444ed1cff7
ayushsingh-22 <ayushkkumar2205@gmail.com>
2024-06-27 16:42:47 +0530

    New

--- classprac.kt
+++ classprac.kt
@@ -1,30 +1,28 @@
-class animal(var name: String, var breed : String, var colour: String, var type: String ){
+class Animal(var name: String, var breed: String, var colour: String, var type: String) {
 
-    fun store_data(name: String, breed : String, colour: String, type: String){
-
+    fun store_data(name: String, breed: String, colour: String, type: String) {
         this.name = name
         this.breed = breed
         this.colour = colour
         this.type = type
-        
     }
 
-    fun print_data()
-    {
+    constructor(name2: Int) : this("", "", "", "") {
+        println(name2)
+    }
+
+    fun print_data() {
         println("The name of animal is $name having breed of $breed whose colour is $colour and type of $type animal.")
     }
-    
 }
 
-fun main()
-{
-    var dog = animal("dog","husky","black","pet");
+fun main() {
+    var dog = Animal("dog", "husky", "black", "pet")
     dog.print_data()
 
-    var cat = animal("cat","desi","brown","pet");
+    var cat = Animal("cat", "desi", "brown", "pet")
     cat.print_data()
-    
-    var lion = animal("lion","royal bengal tiger","yellow","wild");
+
+    var lion = Animal("lion", "royal bengal tiger", "yellow", "wild")
     lion.print_data()
-    
 }
