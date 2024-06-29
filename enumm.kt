65129975399695ba2d572fa8f82368ca1a52917b
ayushsingh-22 <ayushkkumar2205@gmail.com>
2024-06-29 12:54:08 +0530

    New

--- enumm.kt
+++ enumm.kt
@@ -1,3 +1,22 @@
-enum class grade(){
+enum class Day(val holiday: Boolean = false) {
+    SUNDAY(true), MONDAY,
+    TUESDAY(true), WEDNESDAY,
+    THURSDAY, FRIDAY,
+    SATURDAY
+}
+
+class Holiday<T>(val item: T) {
+        println(T)
     
+}
+
+fun main() {
+    for (x in Day.values()) {
+        if (x.holiday) {
+            println(x)
+        }
+    }
+
+    val tu = Holiday("my")
+    println("Generic class --> $tu")
 }
