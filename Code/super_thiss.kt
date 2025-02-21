8ef98526a53275dd707bd13a1d6d1fbecd2ed14d
ayushsingh-22 <ayushkumar2205@gmail.comm>
2025-02-21 20:42:50 +0530

    21-02-25

--- Code/super_thiss.kt
+++ Code/super_thiss.kt
@@ -1,41 +1,51 @@
-fun main()
-{
-    println("Human character --> " + human().whole_structure())
-    println("Animal character --> " + animal().whole_structure())
+// class User (val name: String) {
+//     companion object {
+//         fun createGuest() = User("Guest")
+//         fun createUser(name: String) = User(name)
+//     }
+// }
+
+fun main() {
+    // val guest = User.createGuest()
+    // val user = User.createUser("Alice")
+    // println(guest.name)  // Output: Guest
+    // println(user.name)   // Output: Alice
+    // MyClass.Companion.print() 
+
+    // val ludo = ludo.red("Blue", 10)
+    // val point = when(ludo.type) {
+    //     "Red" -> ludo.point*10
+    //     "Green" -> ludo.point*20
+    //     "Yellow" -> ludo.point*30
+    //     "Blue" -> ludo.point*0.5
+    //     else -> 0
+    // }
+
+    // println(point)
+
+    val greet: (String) -> String = { name -> "Hello, $name!" }
+    println(greet("Alice"))  // Output: Hello, Alice!
 
 }
 
-open class structure
-{
-    open var eye = "2"
-    open var ear = "2"
-    open var hand = "2"
-    open var leg = "2"
+interface Printer {
+    fun print()
+}
 
-    init
-    {
-        println("This is basic structure of livivng being who exsist")
-    }
 
-    open fun whole_structure(): String {
-            var printt = ("We are in structure class")
-            return printt
+class MyClass {
+    companion object : Printer {
+        override fun print() {
+            println("Companion printing...")
         }
-}
-
-class human : structure(){
-
-    override fun whole_structure(): String {
-        var face ="Human have diffrent facial structure"
-        return face
     }
 }
 
-class animal : structure(){
 
-    override fun whole_structure(): String {
-        println(super.whole_structure())
-        var wild ="More dangerious than all other species"
-        return wild
-    }
+sealed class ludo {
+    class red (val type: String, val point: Int) : ludo()
+    class green (val type: String, val point: Int) : ludo()
+    class yellow (val type: String, val point: Int) : ludo()
+    class blue (val type: String, val point: Int) : ludo()
 }
+
